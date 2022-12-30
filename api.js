8cdb9c8fc80d671449be428fb5df6298a501ffe2
GirirajPatidar <111531803+GirirajPatidar@users.noreply.github.com>
2022-12-30 16:04:16 +0530

    Update api.js

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ export const database = [
     { name: 'Cars 2', imgUrl: 'https://upload.wikimedia.org/wikipedia/en/7/7f/Cars_2_Poster.jpg' },
     { name: 'Cars 3', imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/p_cars3_19643_3ab8aca1.jpeg' }
 ]
-export const availability = {
+export const availibility = {
     '21 Jump Street': [1, 2, 5, 8, 9,19,22,20,11],
     '22 Jump Street': [1, 2, 3, 5, 6,10,11,12,13,14,15,16,17, 24],
     'Cars': [1, 2, 3, 4, 5, 6,15,16,17,18,19,20,21,22,23,24],
@@ -23,10 +23,12 @@ const fetchMovieList = async () => {
 const fetchMovieAvailability = async (movieName) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            for (const m in availability) {
-                if (m === movieName) resolve(availability[m])
+            for (const m in availibility) {
+                if (m === movieName) resolve(availibility[m])
             }
             resolve([])
         }, 500)
     })
 }
+
+export { fetchMovieList, fetchMovieAvailability }
